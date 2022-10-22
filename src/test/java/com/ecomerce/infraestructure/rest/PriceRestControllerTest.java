5edf1b777f66bb2c4c3c6cf3d0c34183a77dfa8b
Jose A. Luceño <jalucenyo@gmail.com>
2022-10-22 22:51:13 +0200

    add application from testing

--- src/test/java/com/ecomerce/infraestructure/rest/PriceRestControllerTest.java
+++ src/test/java/com/ecomerce/infraestructure/rest/PriceRestControllerTest.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -17,6 +18,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
 @AutoConfigureMockMvc
 public class PriceRestControllerTest {
 
